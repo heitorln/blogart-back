cebda2ee924c973ea87ba9f09606fe4c4523a50e
heitorln <heitorln@LAPTOP_TOI>
2023-09-15 00:52:41 -0300

    1st commit

---
+++ src/main/java/com/example/blogart/BlogartApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogartApplication.class, args);
+	}
+
+}
